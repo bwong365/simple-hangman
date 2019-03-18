fe1a145a6df72adb81257dbe9b5d4a93f605d421
Ben Wong <bwong365@outlook.com>
2019-03-17 19:53:39 -0600

    added Dictionary class

---
+++ Dictionary.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Random;
+
+public class Dictionary {
+    private List<String> words;
+    private Random random;
+
+    public Dictionary() throws IOException {
+        words = Files.readAllLines(Paths.get("dictionary.txt"));
+        random = new Random();
+    }
+
+    public String random() {
+        int line = this.random.nextInt(words.size());
+        return words.get(line);
+    }
+}
--- Main.java
+++ Main.java
@@ -1,2 +1,15 @@
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        Dictionary dictionary = new Dictionary();
+        String test = dictionary.random();
+        for (int i = 0; i < 10; i++) {
+            System.out.println(dictionary.random());
+        }
+
+        // Get a list of words
+        // Logic
+    }
 }
+
